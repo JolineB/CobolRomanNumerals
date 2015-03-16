a6bdbefb60d9e002d05b2259354f2d9c4bd981bb
Joline Barrett <jolinebarrett@Jolines-MacBook-Pro.local>
2015-03-16 13:19:23 -0400

    test if for 'I'

--- conv.cob
+++ conv.cob
@@ -31,7 +31,8 @@ PROCEDURE DIVISION USING ARRAY-AREA, M, ERR, SUM1.
     MOVE 1 TO ERR. GO TO B8.
 LOOP.
     IF S(I) = 'I'
-    MOVE 1 TO D. perform 3.
+    MOVE 1 TO D 
+    perform 3
     end-if.
 B1. IF S(I) NOT = 'V' GO TO B2 end-if.
     MOVE 5 TO D. perform 3.
