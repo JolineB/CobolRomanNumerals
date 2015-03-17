432388ce879d9bd79bc7600c0a94ef2ff9201307
Joline Barrett <jolinebarrett@Jolines-MacBook-Pro.local>
2015-03-17 18:01:42 -0400

    added perform B3

--- romanA3_1.cob
+++ romanA3_1.cob
@@ -52,7 +52,7 @@ PROCEDURE DIVISION.
     WRITE STDOUT-RECORD FROM UNDERLINE-2 AFTER ADVANCING 1 LINE.
 L1. MOVE 1 TO N. MOVE SPACES TO ARRAY-AREA.
 L2. accept STANDARD-INPUT.
-    read STANDARD-INPUT into inputNum.
+    read STANDARD-INPUT into inputNum at end perform B3.
     move inputNum to ARRAY-AREA.
     inspect inputNum tallying total for all characters before initial '\0'.
     inspect inputNum tallying len for trailing spaces.
